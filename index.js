1ed9b7d3e63d31cee28450f8dd177e55e645b195
boocipher <boocipher@gmail.com>
2022-02-07 01:20:25 -0600

    adds index.js and dir structurel

---
+++ index.js
@@ -0,0 +1,34 @@
+const questions = require('./main/lib/questions');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+const db 
+
+// I am presented with the following options: 
+// view all departments,
+// const showDepartments
+
+// view all roles, 
+// const showRoles
+
+// view all employees, 
+// const showEmployees
+
+// add a department, 
+// const addDepartment
+
+// add a role, 
+// const addRole
+
+// add an employee, and 
+// const assEmployee
+
+// update an employee role
+//  const updateEmployee
+
+
+// -- * BONUS: Update employee managers.
+// -- * BONUS: View employees by manager.
+// -- * BONUS: View employees by department.
+// -- * BONUS: Delete departments, roles, and employees.
+// -- * BONUS: View the total utilized budget of a department&mdash;
+// -- in other words, the combined salaries of all employees in that department.
