5211af73b9b5eb284e91fc3dd46668b62ae42e3e
boocipher <boocipher@gmail.com>
2022-02-06 23:51:36 -0600

    adds schema.sql

---
+++ schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE departments (
+    department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE roles (
+    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary INT NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(department_id) ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+    emp_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT UNSIGNED,
+    INDEX man_ind (manager_id),
+    FOREIGN KEY (manager_id) REFERENCES employees(employee_id) ON DELETE SET NULL,
+    FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE SET NULL
+);
