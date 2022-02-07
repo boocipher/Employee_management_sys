e57656142fc7242483693df4653697c85fefb597
boocipher <boocipher@gmail.com>
2022-02-06 23:56:49 -0600

    adds seeds.sql

---
+++ seeds.sql
@@ -0,0 +1,11 @@
+USE company_db
+
+INSERT INTO departments (department_name)
+VALUES 
+    ('IT'),
+    ('Human Resources'),
+    ('Research and Dev'),
+    ('Sales'),
+    ('Operations');
+    ('Legal');
+
