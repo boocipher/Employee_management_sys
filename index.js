368841c93bcce4af371631e18bcc90bcac326b1e
boocipher <boocipher@gmail.com>
2022-03-01 23:22:37 -0600

    Adds addDept, viewRoles, addRole, viewEmps

--- index.js
+++ index.js
@@ -1,45 +1,17 @@
-const questions = require('./lib/questions');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
-const Queries = require('./lib/queries')
+const Queries = require('./lib/queries');
+const Questions = require('./lib/questions');
 
 // new object of class Queries
-const q = new Queries();
-q.viewDepartments();
+const queries = new Queries();
+
+const q = new Questions(queries);
+
 
 function init() {
-    inquirer.prompt(questions)
-}
+    q.questions();
+};
 
 init();
 
-
-// I am presented with the following options: 
-// view all departments,
-// const showDepartments
-
-// view all roles, 
-// const showRoles
-
-// view all employees, 
-// const showEmployees
-
-// add a department, 
-// const addDepartment
-
-// add a role, 
-// const addRole
-
-// add an employee, and 
-// const assEmployee
-
-// update an employee role
-//  const updateEmployee
-
-
-// -- * BONUS: Update employee managers.
-// -- * BONUS: View employees by manager.
-// -- * BONUS: View employees by department.
-// -- * BONUS: Delete departments, roles, and employees.
-// -- * BONUS: View the total utilized budget of a department&mdash;
-// -- in other words, the combined salaries of all employees in that department.
